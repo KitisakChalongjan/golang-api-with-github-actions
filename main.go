48a69610b5fa0dce988b13589a62b9dc65c661d4
kitisak.c <kitisak.chalongjan@siamsmartsolutions.com>
2024-09-26 10:25:13 +0700

    update program to use .env file

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/goburrow/modbus"
+	"github.com/joho/godotenv"
 )
 
 type ModbusData struct {
@@ -22,15 +25,16 @@ func main() {
 
 	log.Println("Starting the application...")
 
-	machine1IP := "192.168.1.33"
-	machine1Port := "501"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 
-	machine2IP := "192.168.1.33"
-	machine2Port := "502"
+	appPort := os.Getenv("APP_PORT")
 
 	modbusData := &ModbusData{}
 
-	go backgroundTask(modbusData, machine1IP, machine1Port, machine2IP, machine2Port)
+	go backgroundTask(modbusData)
 
 	router := gin.Default()
 
@@ -42,7 +46,7 @@ func main() {
 
 	router.GET("/modbus-data", getModbusDataHandler(modbusData))
 
-	router.Run("0.0.0.0:8000")
+	router.Run(fmt.Sprintf("0.0.0.0:%s", appPort))
 }
 
 func getModbusDataHandler(modbusData *ModbusData) gin.HandlerFunc {
@@ -65,20 +69,75 @@ func getModbusDataHandler(modbusData *ModbusData) gin.HandlerFunc {
 	}
 }
 
-func backgroundTask(modbusData *ModbusData, machine1IP, machine1Port, machine2IP, machine2Port string) {
+func backgroundTask(modbusData *ModbusData) {
+
+	machine1IP := os.Getenv("MACHINE_1_IP")
+	machine1Port := os.Getenv("MACHINE_1_PORT")
+	machine1Start, err := strconv.Atoi(os.Getenv("MACHINE_1_START_ADDRESS"))
+	if err != nil {
+		log.Printf("Cannot parse \"machine1Start\" to int")
+		return
+	}
+	machine1Count, err := strconv.Atoi(os.Getenv("MACHINE_1_READ_COUNT"))
+	if err != nil {
+		log.Printf("Cannot parse \"machine1Count\" to int")
+		return
+	}
+
+	machine2IP := os.Getenv("MACHINE_2_IP")
+	machine2Port := os.Getenv("MACHINE_2_PORT")
+	machine2Start, err := strconv.Atoi(os.Getenv("MACHINE_2_START_ADDRESS"))
+	if err != nil {
+		log.Printf("Cannot parse \"machine2Start\" to int")
+		return
+	}
+	machine2Count, err := strconv.Atoi(os.Getenv("MACHINE_2_READ_COUNT"))
+	if err != nil {
+		log.Printf("Cannot parse \"machine2Count\" to int")
+		return
+	}
+
+	handler1, err1 := createModbusHandler(machine1IP, machine1Port)
+	if err1 != nil {
+		log.Printf("Error creating handler for machine 1: %v", err1)
+		return
+	}
+	defer handler1.Close()
+
+	handler2, err2 := createModbusHandler(machine2IP, machine2Port)
+	if err2 != nil {
+		log.Printf("Error creating handler for machine 2: %v", err2)
+		return
+	}
+	defer handler2.Close()
+
+	client1 := modbus.NewClient(handler1)
+	client2 := modbus.NewClient(handler2)
+
+	isUseModbus, err := strconv.ParseBool(os.Getenv("USE_MODSIM"))
+	if err != nil {
+		log.Printf("Error convert isUseModbus to bool")
+		return
+	}
+
+	if isUseModbus {
+		machine1Start -= 1
+		machine2Start -= 1
+	}
+
 	for {
 
-		data1, err1 := readModbusData(machine1IP, machine1Port)
+		data1, err1 := readModbusData(client1, machine1Start, machine1Count)
 		if err1 != nil {
 			log.Printf("Error reading from machine 1: %v", err1)
 		}
 
-		data2, err2 := readModbusData(machine2IP, machine2Port)
+		data2, err2 := readModbusData(client2, machine2Start, machine2Count)
 		if err2 != nil {
 			log.Printf("Error reading from machine 2: %v", err2)
 		}
 
-		// fmt.Printf("Data1 : %d\nData2 : %d\n", data1, data2)
+		fmt.Printf("Data1 : %d\nData2 : %d\n", data1, data2)
 
 		modbusData.mux.Lock()
 		modbusData.modbus1Data = data1
@@ -88,22 +147,23 @@ func backgroundTask(modbusData *ModbusData, machine1IP, machine1Port, machine2IP
 		time.Sleep(5 * time.Second)
 	}
 }
-
-func readModbusData(ip string, port string) ([]int64, error) {
+func createModbusHandler(ip, port string) (*modbus.TCPClientHandler, error) {
 	handler := modbus.NewTCPClientHandler(fmt.Sprintf("%s:%s", ip, port))
 	handler.Timeout = 10 * time.Second
 	handler.SlaveId = 1
 
 	err := handler.Connect()
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to %s: %v", ip, err)
+		return nil, fmt.Errorf("failed to connect TCPClient")
 	}
-	defer handler.Close()
 
-	client := modbus.NewClient(handler)
+	return handler, nil
+}
+
+func readModbusData(client modbus.Client, start, count int) ([]int64, error) {
 	results, err := client.ReadHoldingRegisters(99, 10)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read registers from %s: %v", ip, err)
+		return nil, fmt.Errorf("failed to read registers")
 	}
 
 	intResults := convertByteToInt(results)
